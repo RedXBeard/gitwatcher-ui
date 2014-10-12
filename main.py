a3a37b6dd64ade8747361e0c77750a3d0d00a82d
Barbaros Yıldırım <barbarosaliyildirim@gmail.com>
2014-10-12 22:08:14 +0300

    baby steps #2, filechooser added for repo, repolist displayed, adding repo developed

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os, json
+from subprocess import Popen, PIPE
 from kivy.app import App
 from kivy.uix.widget import Widget, ObjectProperty
 from kivy.uix.button import Button
@@ -8,13 +10,15 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
 
+# basename `git rev-parse --show-toplevel`
+REPOFILE = "~/.kivyrepowatcher/repowatcher"
 
 class RepoItem(BoxLayout):
     repo_name = StringProperty()
     repo_path = StringProperty()
     repo_index = NumericProperty()
 
-class CustomButton(Button):
+class MenuButton(Button):
     def on_press(self):
         if self.state == "down":
             if self.uid != self.parent.addrepo.uid:
@@ -28,13 +32,37 @@ class CustomButton(Button):
                 if obj.uid != self.uid:
                     obj.background_color = 2,2,2,9
 
-            # if self.uid == self.parent.addrepo.uid:
-            #     content = Button(text='Close me!')
-            #     popup = Popup(content=content,
-            #                   title='Add Existing Repo',
-            #                   size_hint=(None, None), size=(400, 400))
-            #     content.bind(on_press=popup.dismiss)
-            #     popup.open()
+class AddRepoButton(Button):
+    def on_press(self):
+        if self.text == "Add Repo":
+            self.parent.parent.repopath.text
+            self.parent.parent.popup.dismiss()
+        elif self.text == "Choose" and self.parent.listview.selection:
+            selection = self.parent.listview.selection[0]
+            directory = os.path.dirname(REPOFILE)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            try:
+                repofile = file(REPOFILE, "r")
+            except:
+                repofile = file(REPOFILE, "w")
+            try:
+                data = json.loads(repofile.read())
+            except: data=[]
+            repofile.close()
+            repofile = file(REPOFILE, "w")
+            os.chdir(selection)
+            if os.path.exists(".git"):
+                cmd = "basename `git rev-parse --show-toplevel`"
+                p = Popen(cmd , shell=True, stdout=PIPE, stderr=PIPE)
+                out, err = p.communicate()
+                repo_name = out.rstrip()
+                repo_path = selection
+                data.append({"name": repo_name,
+                             "path": repo_path})
+                json_result = json.dumps(data)
+                repofile.write(json_result)
+            repofile.close()
 
 class RepoWatcher(GridLayout):
     data = ListProperty()
@@ -45,16 +73,22 @@ class RepoWatcher(GridLayout):
             'repo_path': item['path'],
             'repo_name': item['name']}
 
+    def load_data(self):
+        try:
+            repofile = file(REPOFILE, "r")
+            self.data = json.loads(repofile.read())
+        except: self.data=[]
+
 class RepoWatcherApp(App):
 
     def build(self):
         self.title= "Repo Watcher"
         layout = RepoWatcher()
-        layout.data = [{"path":"/Users/denizci/.virtualenvs/RomanAlphabet/project/RomanNumbers", "name":"RomanNumbers"},
-                       {"path":"/Users/denizci/.virtualenvs/sqldict/project/python-sqldict","name":"sqldict"},
-                       {"path":"/Users/denizci/.virtualenvs/django_maskurl/project/django_maskurl","name":"django-maskurl"}]
-        #layout.add_repos()
+        layout.load_data()
         return layout
+
+    def load_data(self):
+        self.layout.load_data()
 
 if __name__ == '__main__':
     RepoWatcherApp().run()
