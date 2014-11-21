eb4b2ef72d584cafa2910bd3ddab670f5fe94fbf
Barbaros Yildirim <barbaros.yildirim@markafoni.com>
2014-11-21 14:27:17 +0200

    main.py was not called outside of the repo folder caused by the path names of fonts. paths are now given as full path

--- settings.py
+++ settings.py
@@ -2,15 +2,20 @@ import os
 from kivy.core.text import LabelBase
 from shortcuts import run_syscall
 
+PROJECT_PATH = "/".join(os.path.realpath(__file__).split("/")[:-1])
+
 KIVY_FONTS = [
     {
         "name": "FiraSans",
-        "fn_regular": "assets/fonts/FiraSans-Regular.ttf",
-        "fn_bold": "assets/fonts/FiraSans-Bold.ttf",
+        "fn_regular": "%s/assets/fonts/FiraSans-Regular.ttf"%PROJECT_PATH,
     },
     {
         "name": "WebAwesome",
-        "fn_regular": "assets/fonts/fontawesome-webfont.ttf",
+        "fn_regular": "%s/assets/fonts/fontawesome-webfont.ttf"%PROJECT_PATH,
+    },
+    {
+        "name": "FiraSansBold",
+        "fn_regular": "%s/assets/fonts/FiraSans-Bold.ttf"%PROJECT_PATH,
     }
 ]
 
@@ -19,7 +24,7 @@ for font in KIVY_FONTS:
 
 KIVY_DEFAULT_FONT = "FiraSans"
 KIVY_ICONIC_FONT = "WebAwesome"
-PROJECT_PATH = "/".join(os.path.realpath(__file__).split("/")[:-1])
+KIVY_DEFAULT_BOLD_FONT = "FiraSansBold"
 
 cmd = "echo $HOME"
 out = run_syscall(cmd)
